dc1149cd26a1da9f9f5d95fc0efa8441effe1074
jacob-wj <wangjia199055@126.com>
2015-04-27 18:27:09 +0800

    init project

---
+++ app/src/main/java/com/jacob/floatview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.jacob.floatview;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    public void showWindow(View view){
+
+    }
+
+    public void dismissWindow(View view){
+
+    }
+
+}
